2c5868cff78eca6db68faeb275c1d99792e38f00
Husslenomics <huss.trowery@gmail.com>
2017-03-21 00:14:03 -0400

    Started ViewController lifecycle

--- Billy Goat/InitialViewController.swift
+++ Billy Goat/InitialViewController.swift
@@ -11,4 +11,26 @@ import UIKit
 
 final class InintialViewContoller: UIViewController {
     
+    // ViewController Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureCalendarButton()
+        
+        navigationItem.title = "Billy Goat"
+    }
+    
+    @IBAction func showIncomeList(_ sender: Any) {
+    }
+
+    private func configureCalendarButton() {
+        let calendarButton = UIBarButtonItem(title: "Calendar", style: .plain, target: self, action: #selector(InintialViewContoller.calendarButtonPressed))
+        
+        navigationItem.rightBarButtonItem = calendarButton
+    }
+    
+    // Selector
+    func calendarButtonPressed() {
+        print("Calendar button pressed")
+    }
 }
