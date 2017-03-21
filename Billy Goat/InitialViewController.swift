d04b81e0bab95d93efe41ea968dc059a40db345e
Husslenomics <huss.trowery@gmail.com>
2017-03-21 00:30:45 -0400

    added UIButton functions

--- Billy Goat/InitialViewController.swift
+++ Billy Goat/InitialViewController.swift
@@ -11,26 +11,43 @@ import UIKit
 
 final class InintialViewContoller: UIViewController {
     
+    // UI Properties
+    @IBOutlet weak var expenseListButton: UIButton!
+    
     // ViewController Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureCalendarButton()
+        configureExpenseListButton()
         
         navigationItem.title = "Billy Goat"
     }
     
-    @IBAction func showIncomeList(_ sender: Any) {
-    }
-
     private func configureCalendarButton() {
-        let calendarButton = UIBarButtonItem(title: "Calendar", style: .plain, target: self, action: #selector(InintialViewContoller.calendarButtonPressed))
+        let calendarButton = UIBarButtonItem(title: "Calendar", style: .plain, target: self, action: #selector(calendarButtonPressed))
         
         navigationItem.rightBarButtonItem = calendarButton
     }
     
+    private func configureExpenseListButton() {
+        expenseListButton.addTarget(self, action: #selector(expenseListPressed), for: .touchUpInside)
+    }
+    
+    
+    // UI Action
+    @IBAction func showIncomeList(_ sender: Any) {
+        
+    }
+
     // Selector
     func calendarButtonPressed() {
         print("Calendar button pressed")
     }
+    
+    func expenseListPressed() {
+        print("Expense button pressed")
+        
+    }
+    
 }
