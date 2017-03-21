f111f68e167dd21f50efc5748115aa900ac224ff
Husslenomics <huss.trowery@gmail.com>
2017-03-20 23:43:15 -0400

    Added Initial View and View Controller

---
+++ Billy Goat/InitialViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InitialViewController.swift
+//  Billy Goat
+//
+//  Created by Hussle on 3/20/17.
+//  Copyright © 2017 Husslenomics. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class InintialViewContoller: UIViewController {
+    
+}
