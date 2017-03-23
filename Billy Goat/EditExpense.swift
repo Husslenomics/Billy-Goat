cd2e3ba7500d9fe114e5de576ca16178a0b09317
Husslenomics <huss.trowery@gmail.com>
2017-03-23 00:33:39 -0400

    started Edit Expense View

---
+++ Billy Goat/EditExpense.swift
@@ -0,0 +1,25 @@
+//
+//  EditExpense.swift
+//  Billy Goat
+//
+//  Created by Hussle on 3/22/17.
+//  Copyright © 2017 Husslenomics. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class EditExpense: UITableViewController {
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+ 
+    
+}
