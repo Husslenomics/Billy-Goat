8311e01c880f45b5191a3d3ce45c22cda766d8e4
Husslenomics <huss.trowery@gmail.com>
2017-03-30 00:41:47 -0400

    text field fixed

--- Billy Goat/EditExpenseTableViewController.swift
+++ Billy Goat/EditExpenseTableViewController.swift
@@ -16,13 +16,31 @@ protocol EditExpenseTableViewControllerDelegate: class {
 
 class EditExpenseTableViewController: UITableViewController {
     
+    // 2. add property observer to set the state that was created in step 1
+    var expense: Expense?
     weak var editExpenseDelegate: EditExpenseTableViewControllerDelegate?
+    
+    // 1. Create a state tracking with either bool or enum
     
     // UIProperties
     @IBOutlet weak var expenseTextField: UITextField!
     @IBOutlet weak var companyTextField: UITextField!
     @IBOutlet weak var duedateTextField: UITextField!
     @IBOutlet weak var amountTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // check if expense exist (use guard let)
+        guard let expense = expense else { return }
+        
+        // set each textfield's text property with expense data
+        expenseTextField.text = expense.name
+        companyTextField.text = expense.company
+        duedateTextField.text = expense.dueDate
+        amountTextField.text = expense.money
+        
+    }
     
     // IBACtion
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
@@ -32,8 +50,11 @@ class EditExpenseTableViewController: UITableViewController {
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         // FIXME: form validation 
 
+        // 3. Check the state and act accordingly to the state.
         let newExpense = Expense(name: expenseTextField.text ?? "", company: companyTextField.text ?? "", money: amountTextField.text ?? "", dueDate: duedateTextField.text ?? "")
         
+        
+        // 4. You could possible have 2 different delegate or you could pass the state into this delegate method (parameter) and act accordingly in the other view controller. 
         editExpenseDelegate?.didPressDone(with: newExpense)
         
         dismiss(animated: true, completion: nil)
--- Billy Goat/ExpectedExpensesViewController.swift
+++ Billy Goat/ExpectedExpensesViewController.swift
@@ -11,12 +11,12 @@ import UIKit
 class ExpectedExpensesViewController: UITableViewController {
     
     fileprivate var fakeData = Expense.fakeData
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
+    
     
     // number of rows will eventually be indexPath.rows
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -38,17 +38,17 @@ class ExpectedExpensesViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             print("delete")
-    
+            
             // You don't need self here. - why not?
             fakeData.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .automatic)
         }
     }
-
+    
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        tableView.deselectRow(at: indexPath, animated: true)
+        
         
         // Sender should just be self - fixed
         performSegue(withIdentifier: "showEdit", sender: self)
@@ -57,23 +57,48 @@ class ExpectedExpensesViewController: UITableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // reduce indentation and force unwrapping by using guard.
-//        if segue.identifier == "showEdit" {
-//            let controller = segue.destination as! UINavigationController
-//            let firstV = controller.viewControllers[0] as! EditExpenseTableViewController
-//            firstV.editExpenseDelegate = self
-//
-//        }
+        //        if segue.identifier == "showEdit" {
+        //            let controller = segue.destination as! UINavigationController
+        //            let firstV = controller.viewControllers[0] as! EditExpenseTableViewController
+        //            firstV.editExpenseDelegate = self
+        //
+        //        }
         
-        guard segue.identifier == "showEdit" else { return }
-        guard let navigationController = segue.destination as? UINavigationController, let editExpenseTableVC = navigationController.topViewController as? EditExpenseTableViewController else { return }
+        //    guard segue.identifier == "showAdd" else { return }
+        //  guard let navigationController = segue.destination as? UINavigationController, let editExpenseTableVC = navigationController.topViewController as? EditExpenseTableViewController else { return }
         
         
-        editExpenseTableVC.editExpenseDelegate = self
+        //editExpenseTableVC.editExpenseDelegate = self
+        
+        guard let navigationController = segue.destination as? UINavigationController, let editExpenseTableVC = navigationController.topViewController as? EditExpenseTableViewController, let identifier = segue.identifier else { return }
+        
+        
+        switch identifier {
+        case "showEdit":
+            // Find out index Path
+            guard let selectedExpenseIndexPath = tableView.indexPathForSelectedRow else { return }
+        
+            // Get expense data for the indexpath
+            let expense = fakeData[selectedExpenseIndexPath.row]
+            
+            // send the data to editExpenseTableVC
+            editExpenseTableVC.expense = expense
+            editExpenseTableVC.editExpenseDelegate = self
+            editExpenseTableVC.title = "Edit Expense"
+            
+        case "showAdd":
+            editExpenseTableVC.editExpenseDelegate = self
+            editExpenseTableVC.title = "Add Expense"
+            
+        default: break
+        }
+        
     }
 }
 
 extension ExpectedExpensesViewController: EditExpenseTableViewControllerDelegate {
     func didPressDone(with expense: Expense) {
+        // 5. depending on the state either append or insert at certain indexPath. 
         fakeData.append(expense)
         tableView.reloadData()
     }
